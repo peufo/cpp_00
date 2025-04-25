b51f2f2b47bea40382a11610a3bf257946b5c8c4
jonas <jonas.voisard@gmail.com>
2025-04-25 10:44:05 +0200

    ex00 ok

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+static std::string to_upper(std::string str)
+{
+	for (std::string::iterator it = str.begin(); it != str.end(); it++)
+		if ('a' <= *it && *it <= 'z')
+			*it -= 32;
+	return (str);
+}
+
+int main(int ac, char **av)
+{
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+
+	for (int i = 1; i < ac; i++)
+		std::cout << to_upper(av[i]);
+	std::cout << std::endl;
+	return (0);
+}
